664c8072963e88395f5f592c34049c07b1b09f9a
Bruno Buriolla <b.buriolla@gmail.com>
2016-11-11 12:48:50 -0200

    Alterando arquivos para o portifolio.

--- js/angular/indexCtrl.js
+++ js/angular/indexCtrl.js
@@ -1,13 +1,26 @@
-'use strict';
-app.controller('indexCtrl', function($scope, $translate) {
-    $scope.lang = 'br';
+siteApp.controller('indexCtrl', ['$scope', '$window', '$translate', function ($scope, $window, $translate) {
+   
+    $scope.lang = $window.navigator.language || $window.navigator.userLanguage;
+    $scope.currentLang = $scope.lang;
+
     $scope.changeLanguage = function () {
-        if($scope.lang == 'br') {
-            $translate.use('en');
-            $scope.lang = 'en';
+        if ($scope.currentLang == 'pt-BR') {
+            $translate.use('en-US');
+            $scope.currentLang = 'en-US';
         } else {
-            $translate.use('br');
-            $scope.lang = 'br';
+            $translate.use('pt-BR');
+            $scope.currentLang = 'pt-BR';
         }
     };
-});
+
+    function setLanguage() {
+
+        if ($scope.lang != 'pt-BR') {
+            $translate.use('en-US');
+        } else {
+            $translate.use($scope.lang);
+        }
+    }
+
+    setLanguage();
+}]);
--- js/angular/siteApp.js
+++ js/angular/siteApp.js
@@ -1,8 +1,7 @@
-'use strict';
-var app = angular.module('app', ['ngSanitize', 'pascalprecht.translate']);
+var siteApp = angular.module('siteApp', ['ngSanitize', 'ngCookies', 'pascalprecht.translate']);
 
-app.config(function ($translateProvider) {
-    $translateProvider.translations('en', {
+siteApp.config(['$translateProvider', function ($translateProvider) {
+    $translateProvider.translations('en-US', {
         TITLE: 'Bruno Buriolla | .NET Software Engineer',
         OCCUPATION: '.NET Software Engineer',
         LANG: 'Português',
@@ -12,7 +11,7 @@ app.config(function ($translateProvider) {
         CONTACT: 'Contact',
         EXPERIENCE: 'Experience',
         RESUME: '<p>Working as a developer on the Microsoft .NET platform since 2007, I had the opportunity to work with lot of technologies and methodologies. The vast majority of projects that I participated were web applications geared mainly for internal use by companies. I started my career as a developer always working with the concepts of OOP(Object Oriented Programming), which I know quite well, as well as different patterns of Web projects and also some frameworks for the development of front-end and mobile applications.</p><p>I worked under the waterfall model of software development, within the concept of software house, following CMMI standards of process maturity for most of my career. More recently I also worked under the Scrum model for software development, which led me to study agile methodologies for software development and made me a real fan of the concepts used in these methodologies. What strikes me in these methodologies are the focus on results, quality, customer satisfaction and also the team spirit.</p><p>By working almost 9 years at software houses, I had the opportunity to work in various market segments, among them: education, market research, retail, advocacy, medical / scientific, government and finance. Always seeking to understand the specific requirements of each of these sectors and adapting expertise and technology to find out the needs in the best way for every new challenge.</p>',
-        ADRESS: '<i class="fa fa-map-marker"></i>  742 Peixoto Gomide Street, São Paulo, Brazil',
+        ADRESS: '<i class="fa fa-map-marker fa-margin"></i>São Paulo, Brazil',
         CONTACT_INFO: 'Contact Info',
         GET_IN_TOUCH: 'Get in Touch',
         NAME_LABEL: 'Name',
@@ -34,10 +33,13 @@ app.config(function ($translateProvider) {
         FATEC_COURSE: 'Technologist in Data Processing - <strong>Graduated:</strong> 2008',
         FATEC_DESCRIPTION: 'I always liked technology, since I was a kid, so i decided to graduate myself in data processing on FATEC Taquaritinga mainly because it&rsquo;s a recognized technology college in São Paulo state. Differently from other colleges, the FATEC courses are focused on preparing their students for the market and not for the academic or research carrer. There I figured out how challenging can be the software development and was there that I discovered what I pretend to do till the end of my carrer.',
         UNINOVE_COURSE: 'Information Technology Management - <strong>Graduated:</strong> 2015',
-        UNINOVE_DESCRIPTION: 'After working in software development during some years, I decided to change a little bit the focus of my career and know more about how the IT management works. Then I started one new graduate, the course "Information Technology Management" at Uninove. This decision really changed my understanding of software development process and helped a lot to shape my professional posture since. Now I can have a better understand about some management decisions that I always questioned before the course.'
-        
+        UNINOVE_DESCRIPTION: 'After working in software development during some years, I decided to change a little bit the focus of my career and know more about how the IT management works. Then I started one new graduate, the course "Information Technology Management" at Uninove. This decision really changed my understanding of software development process and helped a lot to shape my professional posture since. Now I can have a better understand about some management decisions that I always questioned before the course.',
+        REQUIRED_FIELD: 'This field is required.',
+        INVALID_EMAIL: 'Please enter a valid e-mail adress.',
+        EMAIL_SUCCESS: 'E-mail sent correctly.',
+        EMAIL_ERROR: 'Failed to send the e-mail. Please try again later. Feel free to send me a direct e-mail, send it to b.buriolla@gmail.com'
     });
-    $translateProvider.translations('br', {
+    $translateProvider.translations('pt-BR', {
         TITLE: 'Bruno Buriolla | Desenvolvedor .NET',
         OCCUPATION: 'Desenvolvedor .NET',
         LANG: 'English',
@@ -47,7 +49,7 @@ app.config(function ($translateProvider) {
         CONTACT: 'Contato',
         EXPERIENCE: 'Experiência',
         RESUME: '<p>Trabalhando como desenvolvedor nas plataformas Microsoft .NET desde 2007, tive a oportunidade de trabalhar com diversas tecnologias e metodologias. A grande maioria dos projetos dos quais participei foram aplicações web, voltados principalmente para a utilização interna pelas companhias. Iniciei minha carreia como desenvolvedor trabalhando sempre com os conceitos de OOP(Oriented Object Programming), os quais domino, assim como diversos padrões de projetos Web e também alguns frameworks para desenvolvimento de front-end e aplicações mobile.</p><p>Atuei seguindo o modelo cascata de desenvolvimento de software, dentro do conceito de fabrica de software, seguindo padrões CMMI de maturidade de processos durante boa parte de minha carreira. Recentemente trabalhei também sob o modelo Scrum para desenvolvimento de software, o que me levou a estudar metodologias ágeis para desenvolvimento de software e me tornei um verdadeiro fã dos conceitos aplicados nestas metodologias. O que me chama mais atenção nestas metodologias são o foco nos resultados, na qualidade, na satisfação do cliente e também o espirito de equipe. “A falha de um é a falha de todos, e o sucesso de um é o sucesso de todos”.</p><p>Por trabalhar há quase 9 anos em fabricas de software, tive a oportunidade de trabalhar em diversos segmentos de mercado dentre eles: educacional, pesquisa, varejo, advocatício, medico/cientifico, governamental e financeiro. Sempre buscando entender os requisitos específicos de cada um destes setores e adequando conhecimentos técnicos e tecnológicos para atender da melhor maneira as necessidades a cada novo desafio.</p>',
-        ADRESS: '<i class="fa fa-map-marker"></i>  Rua Peixoto Gomide - 742, São Paulo, Brazil',
+        ADRESS: '<i class="fa fa-map-marker fa-margin"></i>São Paulo, Brasil',
         CONTACT_INFO: 'Info. Contato',
         GET_IN_TOUCH: 'Entre em Contato',
         NAME_LABEL: 'Nome',
@@ -55,7 +57,7 @@ app.config(function ($translateProvider) {
         MESSAGE_LABEL: 'Mensagem',
         SUBIMIT_MESSAGE: 'Enviar Mensagem',
         INPUT_NAME: 'Seu Nome',
-        INPUT_EMAIL: 'Seu Endereço de Email',
+        INPUT_EMAIL: 'Seu Endere&#231;o de Email',
         ST_IT_OCCUPATION: 'Desenvolvedor .NET Sênior (02/2015 - Até o momento)',
         ST_IT_DESCRIPTION: 'Responsável pela manutenção de aplicação SOA (Aplicação orientada a serviço) cujo front-end foi desenvolvido em Silverlight (C#) e o back-end desenvolvido utilizando a tecnologia WCF (C#)',
         DIGISYSTEM_OCCUPATION: 'Desenvolvedor .NET Sênior (10/2010 - 02/2015)',
@@ -69,8 +71,12 @@ app.config(function ($translateProvider) {
         FATEC_COURSE: 'Técnologo em Processamento de Dados - <strong>Graduado em:</strong> 2008',
         FATEC_DESCRIPTION: 'Eu sempre gostei de tecnologia, desde que eu era uma criança, então eu decidi me formar em processamento de dados na FATEC Taquaritinga principalmente porque é uma faculdade de tecnologia reconhecida no Estado de São Paulo. Diferentemente de outras faculdades, cursos Fatec estão focados na preparação de seus alunos para o mercado e não para a carreira acadêmica ou de pesquisa. Lá eu descobri como um desafio pode ser o desenvolvimento de software e foi aí que eu descobri o que eu pretendo fazer até o fim da minha carreira.',
         UNINOVE_COURSE: 'Gestão em Tecnologia da Informação - <strong>Graduado em:</strong> 2015',
-        UNINOVE_DESCRIPTION: 'Depois de trabalhar no desenvolvimento de software, durante alguns anos, decidi mudar um pouco o foco da minha carreira e saber mais sobre como funciona o gerenciamento de TI. Então eu comecei uma nova graduação, o curso "Gestão de Tecnologia da Informação" na Uninove. Esta decisão realmente mudou minha compreensão do processo de desenvolvimento de software e ajudou muito a moldar a minha postura profissional desde então. Agora eu posso ter uma melhor compreensão sobre algumas decisões de gestão que eu sempre questionei antes do curso.'
+        UNINOVE_DESCRIPTION: 'Depois de trabalhar no desenvolvimento de software, durante alguns anos, decidi mudar um pouco o foco da minha carreira e saber mais sobre como funciona o gerenciamento de TI. Então eu comecei uma nova graduação, o curso "Gestão de Tecnologia da Informação" na Uninove. Esta decisão realmente mudou minha compreensão do processo de desenvolvimento de software e ajudou muito a moldar a minha postura profissional desde então. Agora eu posso ter uma melhor compreensão sobre algumas decisões de gestão que eu sempre questionei antes do curso.',
+        REQUIRED_FIELD: 'Este campo &#233; obrigat&#243;rio.',
+        INVALID_EMAIL: 'Por favor informe um endere&#231;o de e-mail v&#225;lido',
+        EMAIL_SUCCESS: 'E-mail enviado corretamente.',
+        EMAIL_ERROR: 'Falha ao enviar o e-mail. Por favor, tente novamente mais tarde. Você também por me enviar um e-mail direto, o endereço é: b.buriolla@gmail.com'
     });
-    $translateProvider.preferredLanguage('br');
+    $translateProvider.preferredLanguage('pt-BR');
     $translateProvider.useSanitizeValueStrategy('sanitize');
-});
+}]);
